e200cebd500ce819bb5aea1e9ef587b7473354b5
LeRoy Gardner <leroy.gardner@gmail.com>
2022-08-03 12:35:33 -0500

    Update main.js
    added calc

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+// set variable kelvins to 293
+const kelvin = 0 ;
 
+//set variable celsius
+const celsius = kelvin - 273;
+
+//calculating F
+let fahrenheit = celsius * (9 / 5) + 32;
+console.log(fahrenheit);
+
+//transforming F
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temprature is ${fahrenheit} degrees Fahrenheit.`);
+
+// what is 0 degrees K
